b13c92a96119237e2e80404a96036f9227aea1c1
Felix Keuya <118135634+Keuya@users.noreply.github.com>
2026-07-07 22:28:01 +0300

    Bump service worker cache version for the day-picker update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "athleteos-cache-v4";
+const CACHE_NAME = "athleteos-cache-v5";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./assets/icon.svg"];
 
 self.addEventListener("install", (event) => {
